e04b4276b792ba8c972768b2522afa9bc3c3d1f4
bhattprerna <bhattprerna673@gmail.com>
2026-04-08 13:16:39 +0530

    fix: tags

--- infra/terraform/modules/aws_ecr/variables.tf
+++ infra/terraform/modules/aws_ecr/variables.tf
@@ -15,10 +15,10 @@ variable "env" {
   default = ""
 }
 
-variable "aws_ecr_name" {
-  description = "aws ecr repository name"
-  type        = string
-}
+# variable "aws_ecr_name" {
+#   description = "aws ecr repository name"
+#   type        = string
+# }
 
 variable "aws_repository_image_tag_mutability" {
   default = "MUTABLE"
