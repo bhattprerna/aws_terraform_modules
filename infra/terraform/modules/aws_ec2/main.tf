d1798f35c3c83e39e89c5a11d5b9de7ab934e664
bhattprerna <bhattprerna673@gmail.com>
2026-04-10 11:48:14 +0530

    feat: adding ec2 module

--- infra/terraform/modules/aws_ec2/main.tf
+++ infra/terraform/modules/aws_ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-    ami = var.ami.id
+    ami = var.ami_id
     instance_type = var.instance_type
     subnet_id = var.subnet_id
     vpc_security_group_ids = var.vpc_security_group_ids
